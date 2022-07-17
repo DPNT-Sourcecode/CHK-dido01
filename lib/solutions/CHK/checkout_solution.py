ebff4c6329a8c3c8634e555e33e8d3b9bafd04aa
sbx_user1051 <sbx_user1051@169.254.38.221>
2022-07-17 12:31:02 +0000

    Sun Jul 17 12:31:02 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,8 @@
-
+from data import PRICE_TABLE
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
 def checkout(skus):
     # Question where is the sku -> price -> offer mapping data come from are we supposed to create a dummy dictionary?
     if not skus:
@@ -15,6 +16,19 @@ def checkout(skus):
     if not skus.isalpha():
         # check if it contains any special chars or numbers
         return -1
+    
+    items_mapping = {}
+    for char in skus:
+        try:
+            items_mapping[char] += 1
+        except KeyError:
+            # if item not already in the mapping dictionary initialize it to 1
+            items_mapping[char] = 1
+    
+    print(items_mapping)
 
     return 10
+
+
+checkout("AAA")
 
