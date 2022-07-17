8f7e506605e4faf957a8da481071fc28368e61ba
sbx_user1051 <sbx_user1051@169.254.38.221>
2022-07-17 12:19:44 +0000

    Sun Jul 17 12:19:44 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,7 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    # Question where is the sku -> price -> offer mapping data come from are we supposed to create a dummy dictionary?
+    print (skus)
+    return -1
+
