b63e1dd48610357e49761b4098dbeed38c960a61
sbx_user1051 <sbx_user1051@169.254.38.221>
2022-07-17 12:22:54 +0000

    Sun Jul 17 12:22:54 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,6 +4,14 @@
 # skus = unicode string
 def checkout(skus):
     # Question where is the sku -> price -> offer mapping data come from are we supposed to create a dummy dictionary?
-    print (skus)
-    return -1
+    if not skus:
+        return -1
+    
+    if not skus.isupper():
+        # check if contains only uppercase letters
+        return -1
+
+    if not skus.isalpha():
+    return skus
+
 
