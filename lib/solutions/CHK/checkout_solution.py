1f11c30e0e5a4060628d80f93b92c8b98b3259a8
sbx_user1051 <sbx_user1051@169.254.38.221>
2022-07-17 12:23:37 +0000

    Sun Jul 17 12:23:37 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,12 +6,13 @@ def checkout(skus):
     # Question where is the sku -> price -> offer mapping data come from are we supposed to create a dummy dictionary?
     if not skus:
         return -1
-    
+
     if not skus.isupper():
         # check if contains only uppercase letters
         return -1
 
     if not skus.isalpha():
+        # check if it contains any special chars or numbers
+        return -1
+
     return skus
-
-
