97d2f9c12d857d0b097cdc53deabf047cf4058bb
sbx_user1051 <sbx_user1051@169.254.38.221>
2022-07-17 12:40:02 +0000

    Sun Jul 17 12:40:02 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -25,22 +25,30 @@ def checkout(skus):
             # if item not already in the mapping dictionary initialize it to 1
             items_mapping[char] = 1
 
+    total_price = 0
     for sku, quantity in items_mapping.items():
         try:
             sku_data = PRICE_TABLE[sku]
             if sku_data['offer']:
                 num_of_items, offer_price = sku_data['offer'].split('for')
-                available_offers = quantity // num_of_items
-                remaining_non_offer_skus = quantity % num_of_items
+                num_of_items = int(num_of_items)
+                offer_price = int(offer_price)
+                print(num_of_items, offer_price)
+                available_offers = quantity // num_of_items # find how many times the offer applies
+                remaining_non_offer_skus = quantity % num_of_items # find how many items are left that have a normal price
                 total_price_of_sku = available_offers * offer_price + remaining_non_offer_skus * sku_data['price']
             else:
-                total_price_of_sku = quantity * 
-            
+                total_price_of_sku = quantity * sku_data['price']
         except KeyError:
             # pass as we dont have clear instrucitons of what to do with unknown skus
             pass
-    return 10
+        total_price += total_price_of_sku
 
+    print(total_price_of_sku)
+    return total_price_of_sku
+
+
+checkout("AAAAB")
 
 
 
